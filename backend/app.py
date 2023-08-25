9bde0d488fc64f78f1d3c8fc215ae0ae307da02b
Manav Pandey <manavpandey@hotmail.com>
2023-08-25 11:02:01 -0700

    Fixed flask variable handling, and created chat component

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,16 @@
 from flask import Flask, request, jsonify
-from backend.init_llama import init_llm
+from init_llama import init_llm
 from flask_cors import CORS
 api = Flask(__name__)
 CORS(api)
 
 # Move the initialization of your conversational AI to the global scope (outside the route function)
 # All your initialization code (from the initial code you provided) goes here
-
 is_initialized = False
-
+query_engine = None
 @api.route('/llm/start', methods=['POST'])
 def start_llm_init():
+    global is_initialized
     if not is_initialized:
         global query_engine
         query_engine = init_llm()
@@ -22,10 +22,11 @@ def start_llm_init():
 
 @api.route('/llm/query', methods=['POST'])
 def post_query():
+    global query_engine
     data = request.get_json()
     query_text = data.get('query', '')
     response = query_engine.query(query_text)
-    return jsonify({'response': response})
+    return jsonify({'response': str(response)})
 
 @api.route('/llm/status', methods=['GET'])
 def get_status():
