44921999d0e9b838f7da4743ba32d4bd4491bb3d
Manav Pandey <manavpandey@hotmail.com>
2023-08-26 12:12:06 -0700

    Moved styling to dedicated css module file

--- app/components/ChatComponent.js
+++ app/components/ChatComponent.js
@@ -2,6 +2,7 @@
 import { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
 import { CircularProgress } from '@mui/material';
+import styles from '../Styling/ChatComponent.module.css'
 
 function ChatComponent() {
     const [inputValue, setInputValue] = useState('');
@@ -54,13 +55,7 @@ function ChatComponent() {
 
     if (loading) {
         return (
-            <div style={{ 
-                display: 'flex', 
-                flexDirection: 'column', 
-                justifyContent: 'center', 
-                alignItems: 'center', 
-                height: '100vh' 
-            }}>
+            <div className={styles.loadingContainer}>
                 <h2>Parsing Documents and Generating Embeddings</h2>
                 <CircularProgress />
             </div>
@@ -72,43 +67,34 @@ function ChatComponent() {
             {!isInitialized ? (
                 <div>
                     <input type="file" onChange={e => setFile(e.target.files[0])} />
-                    <button 
-                        onClick={handleInitialize}
-                        style={{ backgroundColor: '#0070f3', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '4px', cursor: 'pointer' }}
-                    >
+                    <button className={styles.initializeButton} onClick={handleInitialize}>
                         Start AI and Digest Documents
                     </button>
                 </div>
             ) : (
-                <div style={{ width: '500px', height: '600px', border: '1px solid #333', overflowY: 'scroll', padding: '20px', backgroundColor: '#181818' }}>
+                <div className={styles.chatContainer}>
                     {messages.map((message, index) => (
-                        <div key={index} style={{ marginBottom: '15px', display: 'flex', flexDirection: 'column' }}>
-                            <strong style={{ marginBottom: '5px', color: '#f1f1f1' }}>{message.sender} &gt;</strong>
-                            <div style={{
-                                padding: '10px',
-                                borderRadius: '10px',
-                                backgroundColor: message.sender === 'User' ? '#0070f3' : '#242424',
-                                color: message.sender === 'User' ? 'white' : '#f1f1f1',
-                                boxShadow: '0 1px 3px rgba(0, 0, 0, 0.2)'
-                            }}>
+                        <div key={index} className={styles.message}>
+                            <strong className={styles.sender}>{message.sender} &gt;</strong>
+                            <div className={message.sender === 'User' ? styles.userMessage : styles.aiMessage}>
                                 {message.text}
                             </div>
                         </div>
                     ))}
                     <div ref={chatEndRef}></div>
-                    <form onSubmit={handleMessageSubmit} style={{ position: 'absolute', bottom: '10px', width: '460px' }}>
+                    <form onSubmit={handleMessageSubmit} className={styles.form}>
                         <input
                             value={inputValue}
                             onChange={e => setInputValue(e.target.value)}
-                            style={{ width: '360px', padding: '10px', marginRight: '10px', borderRadius: '4px', backgroundColor: '#242424', color: 'white', border: '1px solid #333' }}
+                            className={styles.input}
                             placeholder="Type your queries about your document here"
                         />
-                        <button type="submit" style={{ padding: '10px 20px', backgroundColor: '#0070f3', color: 'white', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>Send</button>
+                        <button type="submit" className={styles.sendButton}>Send</button>
                     </form>
                 </div>
             )}
         </div>
     );
-                        }
-                            
-    export default ChatComponent;
+}
+
+export default ChatComponent;
